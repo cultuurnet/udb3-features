5c9ad51a3c1eb1eb15d15d64e22d1a5173bd5ded
Kristof Coomans <kristof@2dotstwice.be>
2016-12-21 16:04:26 +0100

    Initialize behat

---
+++ src/CdbXmlContext.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CultuurNet\UDB3Features;
+
+use Behat\Behat\Context\Context;
+
+class CdbXmlContext implements Context
+{
+
+}
---
+++ src/EventContext.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CultuurNet\UDB3Features;
+
+use Behat\Behat\Context\Context;
+
+class EventContext implements Context
+{
+
+}
